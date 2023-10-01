dd2086df0fe585bdc049ca5561b3507fd2bec0b4
Barberis <juancebarberis@gmail.com>
2023-10-01 20:05:10 -0300

    updated run scripts

--- run-scaleup.sh
+++ run-scaleup.sh
@@ -6,6 +6,6 @@ alias docc="docker-compose -f docker-compose.yml -f node-base.yml"
 # Detect whether the host machine is MacOS and append docker.sock to volume mapping
 # on the cadvisor container
 export MAC_SOCK=$([[ $OSTYPE == 'darwin'* ]] && echo '/docker.sock')
-
+export RP_EXTENSION="_scale_up"
 # Ask docker-compose to start with 3 replicas for the node service
 docc up --scale node=3 --detach
