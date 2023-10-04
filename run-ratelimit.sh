85d5198a09888af3f6f226b374c928624617799f
camaina <mstibel@gmail.com>
2023-10-04 11:46:31 -0300

    exporting dashboards metadata

--- run-ratelimit.sh
+++ run-ratelimit.sh
@@ -7,5 +7,6 @@ alias docc='docker-compose -f docker-compose.yml -f node-base.yml'
 export MAC_SOCK=`[[ $OSTYPE == 'darwin'* ]] && echo '/docker.sock'`
 export ENABLE_RATE_LIMIT=true
 export RP_EXTENSION="_rate_limiting"
+#export RP_EXTENSION="_rate_limiting_quotes"
 # ask docker compose to start with 3 replicas for node service
 docc up --detach
